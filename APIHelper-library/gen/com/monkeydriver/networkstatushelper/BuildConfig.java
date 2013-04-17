c2c8d4a4507443dc25d8e0abcfb46136f1aa85b8
Dante Passera <dante@smashingboxes.com>
2013-04-17 15:28:56 -0400

    renamed directory

---
+++ APIHelper-library/gen/com/monkeydriver/networkstatushelper/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.monkeydriver.networkstatushelper;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
